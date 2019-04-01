e095949f1a8d37692951c20ca8026032ddcc9c20
Filipe Falcão <filipefalcaosantos@gmail.com>
2019-03-31 22:17:12 -0300

    Implement the substitution and time stop criteria.

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 import math
 import random
+import copy
 import nwalign3 as nw
 
 # Global vars
 k = 1.2
 C = 100
 e = 1.5
-N = 1000
+N = 300
 mutation_rate = 0.03
 
 
@@ -42,28 +43,56 @@ def add_spaces(lines):
     return lines
 
 
+# Remove spaces to standardize
+def remove_spaces(pop):
+    for chromosome in pop:
+        for i in range(len(chromosome)):
+            chromosome[i] = chromosome[i].replace(" ", "")
+
+    return pop
+
+
+# Remove useless gaps from an alignment
+def remove_useless_gaps(lines):
+    to_rm = []
+    only_gaps = True
+    m = calc_m(lines)
+
+    # Find columns with gaps only
+    for i in range(m):
+        for j in range(n):
+            if (lines[j][i] != " ") and (lines[j][i] != "-"):
+                only_gaps = False
+
+        if only_gaps:
+            to_rm.append(i)
+        else:
+            only_gaps = True
+
+    # Remove gap-only columns
+    to_rm.reverse()
+    for i in range(len(lines)):
+        line = []
+        for j in range(m):
+            line.append(lines[i][j])
+
+        for j in to_rm:
+            line.pop(j)
+
+        lines[i] = ''.join(line)
+
+    # Return the new matrix
+    return lines
+
+
 # Add the required gaps
 def add_gaps(lines):
+
     # Complete with gaps
     for i in range(len(lines)):
-        diff = m_1 - len(lines[i])
+        diff = calc_m(lines) - len(lines[i])
         for j in range(diff):
             lines[i] += "-"
-
-    # Add the scale factor
-    m = math.ceil(m_1 * k)
-
-    # Add the scale factor columns
-    diff = m - m_1
-    for i in range(diff):
-        for j in range(len(lines)):
-            lines[j] += "-"
-
-    # Add the offset columns
-    offset = math.ceil(m_1 * 0.2)
-    for i in range(offset):
-        for j in range(len(lines)):
-            lines[j] = "-" + lines[j]
 
     return lines
 
@@ -111,7 +140,7 @@ def f_func(lines):
             value += g_func(lines[j][i], lines[j + 1][i])
 
     value = value - 2 * x * (len(lines) - 1) - e * x
-    return value / m
+    return value
 
 
 # Read input file string
@@ -121,9 +150,6 @@ with open("../data/data_1.txt") as f:
 # Get lines
 input_str = input_str.replace(" ", "")
 lines_list = input_str.split("\n")
-
-# Calc m1
-m_1 = calc_m(lines_list)
 
 # Add the gaps
 lines_list = add_gaps(lines_list)
@@ -156,13 +182,18 @@ for c in range(C):
         lines_list_aux.append(alignment)
 
     # Add the chromosome generated and prints it
+    lines_list_aux = add_gaps(lines_list_aux)
     pop.append(lines_list_aux)
     print("\nChromosome " + str(c + 1) + ":")
     print_chromosome(lines_list_aux)
 
 # Repeat N times or until a good solution
 # appears
+best_chromosome = None
 count = 0
+new_pop = []
+print()
+
 while count < N:
 
     # Evaluate all chromosomes
@@ -170,11 +201,9 @@ while count < N:
     for p in pop:
         evals.append(f_func(p))
 
-    # New population
-    new_pop = []
-
     # Repeat C times
-    for i in range(C):
+    for w in range(C):
+
         # Normalize the fitness
         pop_sum = sum(evals)
         evals_aux = []
@@ -184,7 +213,7 @@ while count < N:
         # Build the mating pool
         pool = []
         for i in range(len(pop)):
-            prob = int(evals_aux[i] * 100)
+            prob = math.ceil(evals_aux[i] * 100)
 
             for j in range(prob):
                 pool.append(pop[i])
@@ -192,10 +221,10 @@ while count < N:
         # Randomly select two parents
         p1 = random.choice(pool)
         p2 = random.choice(pool)
-        print("\nParent 1: ")
-        print_chromosome(p1)
-        print("\nParent 2: ")
-        print_chromosome(p2)
+        # print("\nParent 1: ")
+        # print_chromosome(p1)
+        # print("\nParent 2: ")
+        # print_chromosome(p2)
 
         # Select crossover method
         method = random.randint(0, 1)
@@ -206,21 +235,15 @@ while count < N:
         # Apply vertical crossover
         if method == 0:
             rand_v = random.randint(1, m - 1)
-            print("\nSlice vertically by: " + str(rand_v))
+            # print("\nSlice vertically by: " + str(rand_v))
 
             for i in range(len(p1)):
-                print(p1[i][:rand_v] + "|", end="")
-                print(p2[i][rand_v:])
                 child[i] = p1[i][:rand_v] + p2[i][rand_v:]
 
         # Apply horizontal crossover
         else:
             rand_h = random.randint(1, n - 1)
-            print("\nSlice horizontally by: " + str(rand_h))
-            print_chromosome(p1[:rand_h])
-            [print("*", end="") for i in range(m)]
-            print()
-            print_chromosome(p2[rand_h:])
+            # print("\nSlice horizontally by: " + str(rand_h))
             child = p1[:rand_h] + p2[rand_h:]
 
         # Select mutation method and apply
@@ -237,8 +260,7 @@ while count < N:
                     cell_j = random.randint(0, len(child[cell_i]) - 1)
 
                 child[cell_i] = child[cell_i][:cell_j] + child[cell_i][cell_j + 1:]
-                print("\nMutate: removed a gap from (" + str(cell_i + 1) + ", " + str(cell_j + 1) + ")")
-                print_chromosome(child)
+                # print("\nMutate: removed a gap from (" + str(cell_i + 1) + ", " + str(cell_j + 1) + ")")
 
             # Apply addition
             else:
@@ -246,10 +268,32 @@ while count < N:
                 cell_j = random.randint(1, len(child[cell_i]) - 1)
 
                 child[cell_i] = child[cell_i][:cell_j] + "-" + child[cell_i][cell_j:]
-                print("\nMutate: added a gap in (" + str(cell_i + 1) + ", " + str(cell_j + 1) + ")")
-                print_chromosome(child)
+                # print("\nMutate: added a gap in (" + str(cell_i + 1) + ", " + str(cell_j + 1) + ")")
 
         # Add the child to the new population
+        child = add_gaps(child)
+        child = remove_useless_gaps(child)
         new_pop.append(child)
+        # print_chromosome(child)
 
+    # Get the best chromosome
+    best_val = 0
+    best_chromosome = None
+    for chromosome in new_pop:
+        curr_val = f_func(chromosome)
+        if curr_val >= best_val:
+            best_val = curr_val
+            best_chromosome = chromosome
+
+    # Print stats
+    print("Generation " + str(count + 1) + ": " + str(best_val))
+
+    # Update population
+    pop = copy.deepcopy(new_pop)
+    pop = remove_spaces(pop)
+    new_pop = []
     count = count + 1
+
+# Best solution
+print("\nBest solution:")
+print_chromosome(best_chromosome)
